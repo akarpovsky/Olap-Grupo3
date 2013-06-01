e810374a5fc98419ed35623f52b985ce7b2aaa52
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2013-06-01 19:26:46 -0300

    Borro comentario innecesario

--- src/main/java/olap/olap/project/model/db/TableCreator.java
+++ src/main/java/olap/olap/project/model/db/TableCreator.java
@@ -25,31 +25,6 @@ public class TableCreator {
 					.setConnectionManagerWithCredentials(url, user, password);
 			final Connection conn = connectionManager.getConnectionWithCredentials();
 			System.out.println(connectionManager.toString());
-//			if (notif.isNew()) {
-//				statement = conn
-//						.prepareStatement(
-//								"INSERT INTO NOTIFICATIONS("
-//										+ "user_from, user_to, read, type) VALUES (?, ?, ?, ?)",
-//								PreparedStatement.RETURN_GENERATED_KEYS);
-//				statement.setInt(1, notif.getFrom().getId());
-//				statement.setInt(2, notif.getTo().getId());
-//				statement.setBoolean(3, notif.isRead());
-//				statement.setString(4, notif.getType().toString());
-//				statement.execute();
-//				final ResultSet set = statement.getGeneratedKeys();
-//				set.next();
-//				notif.setId(set.getInt("id"));
-//			} else {
-//				statement = conn.prepareStatement("UPDATE NOTIFICATIONS "
-//						+ "SET user_from = ?, user_to = ?, "
-//						+ "read = ?, type = ? " + "WHERE id = ?");
-//				statement.setInt(1, notif.getFrom().getId());
-//				statement.setInt(2, notif.getTo().getId());
-//				statement.setBoolean(3, notif.isRead());
-//				statement.setString(4, notif.getType().toString());
-//				statement.setInt(5, notif.getId());
-//				statement.execute();
-//			}
 			connectionManager.closeConnection(conn);
 	}
 }
