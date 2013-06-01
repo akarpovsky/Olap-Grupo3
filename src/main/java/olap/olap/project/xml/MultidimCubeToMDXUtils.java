741b3755b6104a2345562c12482dddb64c68271e
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2013-06-01 03:45:21 -0300

    PKs in fact table

--- src/main/java/olap/olap/project/xml/MultidimCubeToMDXUtils.java
+++ src/main/java/olap/olap/project/xml/MultidimCubeToMDXUtils.java
@@ -2,6 +2,7 @@ package olap.olap.project.xml;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.Map.Entry;
 
 import olap.olap.project.model.Cube;
 import olap.olap.project.model.Dimension;
@@ -28,29 +29,55 @@ public class MultidimCubeToMDXUtils {
 
 		System.out.println("\n*** Creación de sentencias MDX *** ");
 		/* Itarate over dimensions and create Dimension Tables */
-		for(Dimension dimension : multidim.getDimensions()){
-			createDimensionTable(sb, dimension);
+		for(Entry<String,Dimension> entry: multidim.getCube().getDimensions().entrySet()){
+			createDimensionTable(sb, entry.getKey(), entry.getValue());
 		}
 		
 		createFactTable(sb, multidim.getCube());
 		
-		return sb.toString();
+		return sb.toString().toLowerCase();
 		
 	}
 
 	private static void createFactTable(StringBuilder sb, Cube cube) {
 		sb.append("CREATE TABLE " + cube.getName() + "_fact (\n");
+		sb.append("\n\t/* Measures*/\n\n");
 		for(Measure m: cube.getMeasures()){
 			sb.append("\t\""+ m.getName() + "\" " + (m.getType().equals("string") ? "char[]":m.getType()) + ",\n");
 		}
+		
+		createFactTablePrimaryKeys(sb, cube);
 		sb.append("\n);\n");
 
 	}
 	
-	private static void createDimensionTable(StringBuilder sb, Dimension dimension) {
+	private static void createFactTablePrimaryKeys(StringBuilder sb, Cube cube) {
+		StringBuilder pks = new StringBuilder();
+		sb.append("\n\t/* Primary keys*/\n\n");
+		for(Entry<String,Dimension> entry : cube.getDimensions().entrySet()){
+			Level l = entry.getValue().getLevel();
+			for(Property p : l.getProperties()){
+				if(p.isPK()){
+					pks.append( "\"" + entry.getKey() + "_" + p.getName() + "\"" +",");
+				}
+				sb.append("\t\""+ entry.getKey() + "_" + p.getName() + "\" " + (p.getType().equals("string") ? "char[]":p.getType()) + " NOT NULL,\n");
+			}
+				
+		}
+		
+		// Borro ultima coma
+		pks.deleteCharAt(pks.length()-1);
+		sb.append("\n\tPRIMARY KEY(" + pks + ")\n");
+		
+		
+	}
+
+//	PRIMARY KEY(question_id, tag_id)
+	
+	private static void createDimensionTable(StringBuilder sb, String dimensionName, Dimension dimension) {
 		// Se asume que la tabla no es vacia!
 		
-		sb.append("CREATE TABLE " + dimension.getName() + " (\n");
+		sb.append("CREATE TABLE " + dimensionName + "_" + dimension.getName() + " (\n");
 		
 		// Creo los campos para el nivel inicial
 		createDimensionBasicFields(sb, dimension.getLevel());
